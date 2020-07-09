d3cb41273763e4681bd467cc1c241cd1254cd318
Paul Gey <narpfel@gmx.de>
2020-07-10 01:15:56 +0200

    Generate `kiro.h` before it is used

--- build.rs
+++ build.rs
@@ -6,6 +6,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("cargo:rerun-if-changed={}", entry?.path().to_str().unwrap());
     }
 
+    let crate_dir = std::env::var("CARGO_MANIFEST_DIR")?;
+    cbindgen::generate(crate_dir)?.write_to_file("kiro.h");
+
     cc::Build::new()
         .compiler("clang")
         .flag("-std=c18")
@@ -18,8 +21,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .file(SOURCE_FILE_NAME)
         .compile("libkilo.a");
 
-    let crate_dir = std::env::var("CARGO_MANIFEST_DIR")?;
-
-    cbindgen::generate(crate_dir)?.write_to_file("kiro.h");
     Ok(())
 }
