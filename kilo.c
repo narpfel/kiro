fa384f3725aacc124578d66cce0afe9a928e5398
Paul Gey <narpfel@gmx.de>
2020-07-09 23:19:08 +0200

    RiiR: `main`

--- kilo.c
+++ kilo.c
@@ -1287,22 +1287,3 @@ void initEditor(void) {
     updateWindowSize();
     signal(SIGWINCH, handleSigWinCh);
 }
-
-int cmain(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr,"Usage: kilo <filename>\n");
-        exit(1);
-    }
-
-    initEditor();
-    editorSelectSyntaxHighlight(argv[1]);
-    editorOpen(argv[1]);
-    enableRawMode(STDIN_FILENO);
-    editorSetStatusMessage(
-        "HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
-    while(1) {
-        editorRefreshScreen();
-        editorProcessKeypress(STDIN_FILENO);
-    }
-    return 0;
-}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,45 @@
+use std::ffi::{CString, NulError};
+
 #[link(name = "kilo", kind = "static")]
 extern "C" {
-    fn cmain(argc: libc::c_int, argv: *mut *mut libc::c_char) -> libc::c_int;
+    fn initEditor();
+    fn editorSelectSyntaxHighlight(filename: *mut libc::c_char);
+    fn editorOpen(filename: *mut libc::c_char);
+    fn enableRawMode(fd: libc::c_int);
+    fn editorSetStatusMessage(msg: *const libc::c_char);
+    fn editorRefreshScreen();
+    fn editorProcessKeypress(fd: libc::c_int);
 }
 
-fn main() {
-    let mut args: Vec<_> = std::env::args().collect();
-    let mut c_args: Vec<_> = args.iter_mut().map(|s| s.as_mut_ptr()).collect();
-    let result = unsafe { cmain(c_args.len() as _, c_args.as_mut_ptr() as _) };
-    std::process::exit(result);
+#[derive(Debug)]
+enum KiloErr {
+    IncorrectInvocation,
+    NulError(NulError),
+}
+
+impl From<NulError> for KiloErr {
+    fn from(err: NulError) -> Self {
+        KiloErr::NulError(err)
+    }
+}
+
+type KiloResult<T> = Result<T, KiloErr>;
+
+fn main() -> KiloResult<()> {
+    let mut filename = std::env::args()
+        .nth(1)
+        .ok_or(KiloErr::IncorrectInvocation)?;
+    unsafe {
+        initEditor();
+        editorSelectSyntaxHighlight(filename.as_mut_ptr() as _);
+        editorOpen(filename.as_mut_ptr() as _);
+        enableRawMode(libc::STDIN_FILENO);
+        editorSetStatusMessage(
+            CString::new("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find")?.as_ptr() as _,
+        );
+        loop {
+            editorRefreshScreen();
+            editorProcessKeypress(libc::STDIN_FILENO);
+        }
+    }
 }
